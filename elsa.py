e455f5d9e5fde3f14f7df7f536b3e24d60d8cabc
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-08-28 00:44:41 -0700

    Update elsa.py
    Disclaimer

--- elsa.py
+++ elsa.py
@@ -1,3 +1,4 @@
+Disclaimer: This is 100 percent not my code. This is a snippet I found which 1. makes me happy 2. gives me project inspiration in the capacity of positive reinforcement.
 elsa.penup()
 elsa.forward(90)
 elsa.left(45)
