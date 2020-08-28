17c0fa4de67ddf92f718da986feda7827b8421d2
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-08-27 17:39:48 -0700

    Update appbar.js

--- app/appbar.js
+++ app/appbar.js
@@ -1,1 +1,2 @@
+//This is from the React AppBar API, one of two APIs we haven't discussed in class. It also uses React. Checked that off my list. Wooo! 
 import AppBar from '@material-ui/core/AppBar'
