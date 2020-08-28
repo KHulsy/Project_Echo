4b7908838001dc37a11833deb480972b1767bd8a
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-08-27 19:05:08 -0700

    Create recursion.php
    Recursion. R-e-c-u-r-s-i-o-n. Recursion.

---
+++ recursion.php
@@ -0,0 +1,27 @@
+<?php
+//PHP program to demonstrate//working of recursion
+//Jerome has this under the expectations with no information connected to the link.
+//Seriously. It just says the word Recurion.
+//Evidently, it needs to be included.
+//How is babby formed?
+<?php>
+//PHP program to demonstrate
+//working of recursion
+//function to demonstrate
+//working of recursion
+function(printFun($test)
+{
+if ($test <1)
+return;
+else
+{echo("$test  ");
+//statement 2
+printFun($test-1);
+return;
+}
+//Driver Code 
+$test=3;
+printFun($test);
+//This code contributed by
+//Smitha Dinesh Semwal
+?>
